f4519f03bd997c38c02d4a15df2d843c24cd2ac8
Shaun Prince <suparious@solidrust.net>
2023-04-08 18:21:34 -0700

    Adding nice progress bars

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
+use serde_derive::Deserialize;
+use ssh2::Session;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
-use std::sync::Arc;
-use ssh2::Session;
-use serde_derive::Deserialize;
-use indicatif::{ProgressBar, ProgressStyle};
+use std::error::Error;
+use std::sync::{Arc, Mutex};
 
 #[derive(Debug, Deserialize)]
 struct Config {
@@ -24,75 +25,113 @@ struct Host {
     key_path: Option<String>,
 }
 
-
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)] // Added Clone trait
 struct Packages {
     global: Vec<String>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)] // Added Clone trait
 struct Users {
     global: Vec<String>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)] // Added Clone trait
 struct Services {
     enable: Vec<String>,
     restart: Vec<String>,
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = read_config("config.toml");
 
-    let handles: Vec<_> = config.hosts.iter().map(|host| {
-        let host_name = host.name.clone();
-        let host_config = host.clone();
-        let packages = config.packages.global.clone();
-        let users = config.users.global.clone();
-        let enable = config.services.enable.clone();
-        let restart = config.services.restart.clone();
-
-        tokio::spawn(async move {
-            println!("Connecting to {}", host_name);
-            let session = connect_to_host(&host_config).await;
-            manage_packages(&session, &packages);
-            manage_users(&session, &users);
-            manage_services(&session, &enable, &restart);
-        })
+    let m = MultiProgress::new();
+    let progress_bars: Vec<Arc<Mutex<ProgressBar>>> = config.hosts.iter().map(|host| {
+        let pb = m.add(ProgressBar::new(1));
+        pb.set_style(
+            ProgressStyle::default_bar()
+                .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos}/{len} {msg}")
+                .unwrap()
+                .progress_chars("##-"),
+        );
+        pb.set_message(format!("Connecting to {}", host.name).to_owned());
+        Arc::new(Mutex::new(pb))
     }).collect();
 
-    futures::future::join_all(handles).await;
+    let message = "Linux Fleet Manager - Working...".to_string();
+    // Set the message on the first progress bar only
+    progress_bars[0].lock().unwrap().set_message(message);
 
-    println!("\nDone with all hosts!");
+    let packages_ref = Arc::new(config.packages);
+    let users_ref = Arc::new(config.users);
+    let services_ref = Arc::new(config.services);    
+
+    let handles: Vec<_> = config
+        .hosts
+        .into_iter()
+        .enumerate()
+        .map(|(i, host)| {
+            let host_name = host.name.clone();
+            let host_config = host.clone();
+            let pb = Arc::clone(&progress_bars[i]);
+        
+            let packages_ref = Arc::clone(&packages_ref);
+            let users_ref = Arc::clone(&users_ref);
+            let services_ref = Arc::clone(&services_ref);
+        
+            tokio::spawn(async move {
+                match connect_to_host(&host_config).await {
+                    Ok(session) => {
+                        manage_packages(&session, &packages_ref.global);
+                        manage_users(&session, &users_ref.global);
+                        manage_services(&session, &services_ref.enable, &services_ref.restart);
+                    }
+                    Err(e) => {
+                        eprintln!("Error connecting to host {}: {}", host_name, e);
+                    }
+                }
+                pb.lock().unwrap().finish_with_message(format!("Done with {}", host_name).to_owned());
+                drop(pb);
+            })
+        })        
+        .collect();
+
+    for handle in handles {
+        handle.await.unwrap();
+    }
+
+    println!("Done with all hosts!");
+    Ok(())
 }
 
-async fn connect_to_host(host: &Host) -> Session {
-    let tcp = tokio::net::TcpStream::connect(format!("{}:{}", host.address, host.port)).await.unwrap();
-    let mut session = Session::new().unwrap();
+async fn connect_to_host(host: &Host) -> Result<Session, Box<dyn Error>> {
+    let tcp = tokio::net::TcpStream::connect((host.address.as_str(), host.port)).await?;
+    let mut session = Session::new()?;
     session.set_tcp_stream(tcp);
-    session.handshake().unwrap();
+    session.handshake()?;
 
     if let Some(key_path) = &host.key_path {
         if !key_path.is_empty() {
             let key_path = Path::new(key_path);
             session
-                .userauth_pubkey_file(&host.user, None, key_path, None)
-                .unwrap();
+                .userauth_pubkey_file(&host.user, None, key_path, None)?;
         } else {
-            session.userauth_password(&host.user, &host.password).unwrap();
+            session.userauth_password(&host.user, &host.password)?;
         }
     } else {
-        session.userauth_password(&host.user, &host.password).unwrap();
+        session.userauth_password(&host.user, &host.password)?;
     }
 
     assert!(session.authenticated());
-    session
+    Ok(session)
 }
 
 fn manage_packages(session: &Session, packages: &[String]) {
     let package_list = packages.join(" ");
-    let command = format!("sudo apt-get update && sudo apt-get install -y {}", package_list);
+    let command = format!(
+        "sudo apt-get update && sudo apt-get install -y {}",
+        package_list
+    );
     execute_command(session, &command);
 }
 
@@ -104,33 +143,33 @@ fn manage_users(session: &Session, users: &[String]) {
 }
 
 fn manage_services(session: &Session, enable: &[String], restart: &[String]) {
-  for service in enable {
-      let command = format!("sudo systemctl enable {}", service);
-      execute_command(session, &command);
-  }
+    for service in enable {
+        let command = format!("sudo systemctl enable {}", service);
+        execute_command(session, &command);
+    }
 
-  for service in restart {
-      let command = format!("sudo systemctl restart {}", service);
-      execute_command(session, &command);
-  }
+    for service in restart {
+        let command = format!("sudo systemctl restart {}", service);
+        execute_command(session, &command);
+    }
 }
 
-fn execute_command(session: &Session, command: &str) {
-  let mut channel = session.channel_session().unwrap();
-  channel.exec(command).unwrap();
-  let mut response = String::new();
-  channel.read_to_string(&mut response).unwrap();
-  println!("Output: {}", response);
-  channel.wait_close().unwrap();
-  println!("Status: {}", channel.exit_status().unwrap());
+fn execute_command(session: &Session, command: &str) -> Result<(), Box<dyn Error>> {
+    let mut channel = session.channel_session()?;
+    channel.exec(command)?;
+    let mut response = String::new();
+    channel.read_to_string(&mut response)?;
+    // println!("Output: {}", response);
+    channel.wait_close()?;
+    // println!("Status: {}", channel.exit_status()?);
+    Ok(())
 }
 
 fn read_config<P: AsRef<Path>>(path: P) -> Config {
-  let mut file = File::open(path).expect("Config file not found");
-  let mut contents = String::new();
-  file.read_to_string(&mut contents).expect("Error reading config file");
+    let mut file = File::open(path).expect("Config file not found");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("Error reading config file");
 
-  toml::from_str(&contents).expect("Error parsing config file")
+    toml::from_str(&contents).expect("Error parsing config file")
 }
-
-
